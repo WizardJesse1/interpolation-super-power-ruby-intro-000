9f906e6a70993cf5c956482301a9e301eb9b54aa
Jesse Woolsey <jesse232@live.com>
2020-01-13 03:15:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,30 @@
-# Write your #display_rainbow method here
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
+end
+
+bob     = ["Bob", 46, "Juggler", "555-555-5555"]
+stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
+
+print_business_card(bob)
+
+
+def display_rainbow(color) 
+  puts + color[0] + " " + color[1]
+end
+
+red = ["R: ", 'red']
+orange = ["O: ", 'orange']
+yellow = ["Y: ", 'yellow']
+green = ["G: ", 'green']
+blue = ["B: ", 'blue']
+indigo = ["I: ", 'indigo']
+violet = ["V: ", 'violet']
+
+display_rainbow(red)
+display_rainbow(orange)
+display_rainbow(yellow)
+display_rainbow(green)
+display_rainbow(blue)
+display_rainbow(indigo)
+display_rainbow(violet)
