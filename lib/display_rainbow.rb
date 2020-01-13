648603014fa52d802f89380fb135ccf09f0cdbf2
Jesse Woolsey <jesse232@live.com>
2020-01-13 03:16:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -10,7 +10,7 @@ print_business_card(bob)
 
 
 def display_rainbow(color) 
- puts 
+ puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
